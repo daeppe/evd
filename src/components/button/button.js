d38d97d7291c9f4f8c3d0042f8bcd0e4e1c1ee96
Epichin <d.pena@hotmail.com>
2023-09-04 10:22:02 -0300

    MVP finished.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,8 +4,9 @@ const Button = (props) => {
     const child = props.children;
     const type = props.type;
     const onClick = props.onClick;
+    const isLoading = props.isLoading;
 
-    return <button className={styles['button']} type={type} onClick={onClick}>{child}</button>;
+    return <button className={styles['button']} type={type} onClick={onClick} disabled={isLoading} >{child}</button>;
 };
 
 export default Button;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,10 @@ export default function Home() {
 
   const [urlVideo, setUrlVideo] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Está aqui', urlVideo);
-  };
-
   return (
     <div className={styles['index']}>
       <Header />
-      <Form onSubmit={handleSubmit} urlVideo={urlVideo} setUrlVideo={setUrlVideo} />
+      <Form urlVideo={urlVideo} setUrlVideo={setUrlVideo} />
       <p>Site para baixar videos do youtube nesses formatos.
         <br />Quanto maior o vídeo for mais tempo requer para começar o download.</p>
     </div>
